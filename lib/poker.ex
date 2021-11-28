e5ca63e04185ae948384b811759c52bf44b0911b
Scott Woodall <scott.woodall@gmail.com>
2021-11-27 22:16:10 -0500

    initial commit

---
+++ lib/poker.ex
@@ -0,0 +1,192 @@
+defmodule Poker do
+  @moduledoc """
+  Documentation for `Poker`.
+  """
+
+  @doc """
+  Return a list of winning hands: poker([hand, ...]) => [hand,...]
+  """
+  def play(hands) when is_list(hands) and length(hands) > 0 do
+    allmax(hands)
+  end
+
+  def allmax(hands) do
+    hands
+    |> Enum.map(fn x ->
+      rank = hand_rank(x)
+      {x, rank}
+    end)
+    |> Enum.sort_by(
+      fn x ->
+        {_hand, rank} = x
+        rank
+      end,
+      :desc
+    )
+    |> Enum.reduce_while([], fn x, acc ->
+      {_, rank} = x
+
+      {_, max_rank} =
+        Enum.max_by(
+          acc,
+          fn y ->
+            {_, sort_rank} = y
+            sort_rank
+          end,
+          fn -> {[], {0, 0, 0}} end
+        )
+
+      cond do
+        Enum.count(acc) == 0 or rank >= max_rank ->
+          {:cont, [x | acc]}
+
+        true ->
+          {:halt, acc}
+      end
+    end)
+    |> Enum.map(fn x ->
+      {hand, _} = x
+      hand
+    end)
+  end
+
+  @doc """
+  Returns an unshuffled 52 card deck
+  """
+  def new_deck() do
+    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+    suits = ["S", "H", "D", "C"]
+    for r <- ranks, s <- suits, do: "#{r}#{s}"
+  end
+
+  @doc """
+  Returns a shuffled version of the passed in deck
+  """
+  def shuffle_deck(deck) do
+    Enum.shuffle(deck)
+  end
+
+  @doc """
+  ["KC", "JS", "3S", "KS", "7S", "TD", "2H", "8H", "4C", "AD", "6H"]
+
+  [
+      ["KC", "JS", "3S", "KS", "7S"],
+      ["TD", "2H", "8H", "4C", "AD", "6H"],
+  ]
+  """
+  def deal(num_hands, deck, num_cards \\ 5, accumulator \\ [])
+
+  def deal(num_hands, deck, num_cards, accumulator) when num_hands > 0 do
+    accumulator = [Enum.slice(deck, 0, num_cards) | accumulator]
+    deck = Enum.drop(deck, num_cards)
+    deal(num_hands - 1, deck, num_cards, accumulator)
+  end
+
+  def deal(0, deck, _num_cards, accumulator) do
+    {accumulator, deck}
+  end
+
+  @doc """
+  Return a value indicating the ranking of a hand
+  """
+  def hand_rank(hand) do
+    ranks = card_ranks(hand)
+
+    cond do
+      # straight flush
+      straight?(ranks) and flush?(hand) ->
+        {8, Enum.max(ranks), 0}
+
+      # four of a kind
+      kind?(4, ranks) ->
+        {7, kind(4, ranks), kind(1, ranks)}
+
+      # full house
+      kind?(3, ranks) and kind?(2, ranks) ->
+        {6, kind(3, ranks), kind(2, ranks)}
+
+      # flush
+      flush?(hand) ->
+        {5, ranks, 0}
+
+      # straight
+      straight?(ranks) ->
+        {4, Enum.max(ranks), 0}
+
+      # three of a kind
+      kind?(3, ranks) ->
+        {3, kind(3, ranks), ranks}
+
+      # two pair
+      two_pair(ranks) ->
+        {2, two_pair(ranks), ranks}
+
+      # two of a kind
+      kind(2, ranks) ->
+        {1, kind(2, ranks), ranks}
+
+      # highest card
+      true ->
+        {0, ranks, 0}
+    end
+  end
+
+  def card_ranks(hand) do
+    rankings = [nil, nil, "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+    cards = for card <- hand, do: String.first(card)
+    ranks = for rank <- cards, do: Enum.find_index(rankings, fn x -> x == rank end)
+    sorted = Enum.sort(ranks, :desc)
+
+    cond do
+      # edge case where ace is used in low straight
+      sorted == [14, 5, 4, 3, 2] ->
+        [5, 4, 3, 2, 1]
+
+      true ->
+        sorted
+    end
+  end
+
+  @doc """
+  If there are two pair, return the two ranks as a tuple (highest, lowest);
+  otherwise reuturn nil.
+  """
+  def two_pair(ranks) do
+    pair = kind(2, ranks)
+    low_pair = kind(2, Enum.reverse(ranks))
+
+    if pair != nil and low_pair != pair do
+      {pair, low_pair}
+    else
+      nil
+    end
+  end
+
+  @doc """
+  Return true if the ordered ranks form a 5-card straight
+  """
+  def straight?(ranks) do
+    Enum.sort(ranks) == Enum.to_list(Enum.min(ranks)..Enum.max(ranks))
+  end
+
+  @doc """
+  Return true if all the cards have the same suit
+  """
+  def flush?(hand) do
+    cards = for card <- hand, do: String.last(card)
+    suits = MapSet.new(cards)
+    MapSet.size(suits) == 1
+  end
+
+  def kind(n, ranks) do
+    frequencies = Enum.frequencies(ranks)
+    kinds = Enum.map(ranks, fn x -> {x, Map.get(frequencies, x)} end)
+    bro = Enum.filter(kinds, fn {_, value} -> value == n end)
+    rank = List.first(bro, {nil, nil})
+    Kernel.elem(rank, 0)
+  end
+
+  def kind?(n, ranks) do
+    kind(n, ranks) != nil
+  end
+end
---
+++ test/poker_test.exs
@@ -0,0 +1,220 @@
+defmodule PokerTest do
+  use ExUnit.Case
+
+  test "card ranks for four of a kind" do
+    four_of_a_kind = ["9D", "7D", "9H", "9S", "9C"]
+    expected = [9, 9, 9, 9, 7]
+    actual = Poker.card_ranks(four_of_a_kind)
+    assert actual == expected
+  end
+
+  test "card ranks for straight flush" do
+    straight_flush = ["8C", "6C", "9C", "7C", "TC"]
+    expected = [10, 9, 8, 7, 6]
+    actual = Poker.card_ranks(straight_flush)
+    assert actual == expected
+  end
+
+  test "card ranks for ace low straight" do
+    straight = ["AD", "2C", "3S", "4C", "5C"]
+    expected = [5, 4, 3, 2, 1]
+    actual = Poker.card_ranks(straight)
+    assert actual == expected
+  end
+
+  test "card ranks for full house" do
+    full_house = ["7D", "7C", "TD", "TC", "TH"]
+    expected = [10, 10, 10, 7, 7]
+    actual = Poker.card_ranks(full_house)
+    assert actual == expected
+  end
+
+  test "error thrown when no hands are played" do
+    hands = []
+
+    assert_raise FunctionClauseError, fn ->
+      Poker.play(hands)
+    end
+  end
+
+  test "error thrown when hands is not a list" do
+    hands = "fake data"
+
+    assert_raise FunctionClauseError, fn ->
+      Poker.play(hands)
+    end
+  end
+
+  test "is a straight" do
+    ranks = [9, 8, 7, 6, 5]
+    expected = true
+    actual = Poker.straight?(ranks)
+    assert actual == expected
+  end
+
+  test "is not a straight" do
+    ranks = [9, 8, 8, 6, 5]
+    expected = false
+    actual = Poker.straight?(ranks)
+    assert actual == expected
+  end
+
+  test "is a flush" do
+    hand = ["9S", "8S", "7S", "6S", "5S"]
+    expected = true
+    actual = Poker.flush?(hand)
+    assert actual == expected
+  end
+
+  test "is not a flush" do
+    hand = ["9S", "8S", "7H", "6S", "5S"]
+    expected = false
+    actual = Poker.flush?(hand)
+    assert actual == expected
+  end
+
+  test "is four of a kind" do
+    four_of_a_kind = ["9D", "9H", "9S", "9C", "7D"]
+    ranks = Poker.card_ranks(four_of_a_kind)
+    expected = 9
+    actual = Poker.kind(4, ranks)
+    assert actual == expected
+  end
+
+  test "is four of a kind but tests for three of a kind" do
+    four_of_a_kind = ["9D", "9H", "9S", "9C", "7D"]
+    ranks = Poker.card_ranks(four_of_a_kind)
+    expected = nil
+    actual = Poker.kind(3, ranks)
+    assert actual == expected
+  end
+
+  test "is four of a kind but tests for one of a kind" do
+    four_of_a_kind = ["9D", "9H", "9S", "9C", "7D"]
+    ranks = Poker.card_ranks(four_of_a_kind)
+    expected = 7
+    actual = Poker.kind(1, ranks)
+    assert actual == expected
+  end
+
+  test "is two pair" do
+    hand = ["5S", "5D", "9H", "9C", "6S"]
+    hand_rank = Poker.card_ranks(hand)
+    expected = {9, 5}
+    actual = Poker.two_pair(hand_rank)
+    assert actual == expected
+  end
+
+  test "is not two pair" do
+    hand = ["5S", "5D", "9H", "TC", "6S"]
+    hand_rank = Poker.card_ranks(hand)
+    expected = nil
+    actual = Poker.two_pair(hand_rank)
+    assert actual == expected
+  end
+
+  test "full house hand rank value" do
+    full_house = ["TD", "TC", "TH", "7C", "7D"]
+    expected = {6, 10, 7}
+    actual = Poker.hand_rank(full_house)
+    assert actual == expected
+  end
+
+  test "straight flush hand rank value" do
+    straight_flush = ["6C", "7C", "8C", "9C", "TC"]
+    expected = {8, 10, 0}
+    actual = Poker.hand_rank(straight_flush)
+    assert actual == expected
+  end
+
+  test "straight flush over four of a kind and full house" do
+    straight_flush = ["6C", "7C", "8C", "9C", "TC"]
+    four_of_a_kind = ["9D", "9H", "9S", "9C", "7D"]
+    full_house = ["TD", "TC", "TH", "7C", "7D"]
+    hands = [straight_flush, four_of_a_kind, full_house]
+    expected = [straight_flush]
+    actual = Poker.play(hands)
+    assert actual == expected
+  end
+
+  test "only a single hand playing wins" do
+    four_of_a_kind = ["9D", "9H", "9S", "9C", "7D"]
+    hands = [four_of_a_kind]
+    expected = [four_of_a_kind]
+    actual = Poker.play(hands)
+    assert actual == expected
+  end
+
+  test "four of a kind hand rank value" do
+    four_of_a_kind = ["9D", "9H", "9S", "9C", "7D"]
+    expected = {7, 9, 7}
+    actual = Poker.hand_rank(four_of_a_kind)
+    assert actual == expected
+  end
+
+  """
+  test "straight flush beats 100 full houses" do
+    straight_flush = ["6C", "7C", "8C", "9C", "TC"]
+    full_house = ["TD", "TC", "TH", "7C", "7D"]
+    one_hundred_full_houses = full_house |> List.duplicate(100)
+    hands = [straight_flush | one_hundred_full_houses]
+    expected = [straight_flush]
+    actual = Poker.play(hands)
+    assert actual == expected
+  end
+  """
+
+  test "four of a kind over full house" do
+    four_of_a_kind = ["9D", "9H", "9S", "9C", "7D"]
+    full_house = ["TD", "TC", "TH", "7C", "7D"]
+    hands = [four_of_a_kind, full_house]
+    expected = [four_of_a_kind]
+    actual = Poker.play(hands)
+    assert actual == expected
+  end
+
+  test "ace low straight flush beats four of a kind" do
+    four_of_a_kind = ["9D", "9H", "9S", "9C", "7D"]
+    straight_flush = ["2C", "3C", "4C", "5C", "AC"]
+    hands = [four_of_a_kind, straight_flush]
+    expected = [straight_flush]
+    actual = Poker.play(hands)
+    assert actual == expected
+  end
+
+  test "full house equal to full house" do
+    full_house_p1 = ["TD", "TC", "TH", "7C", "7D"]
+    full_house_p2 = ["TS", "TC", "TH", "7S", "7H"]
+    high_card = ["2S", "5D", "7H", "9S", "JH"]
+    hands = [full_house_p1, full_house_p2, high_card]
+    actual = Poker.play(hands)
+    assert Enum.member?(actual, full_house_p1)
+    assert Enum.member?(actual, full_house_p2)
+    assert Enum.count(actual) == 2
+  end
+
+  test "deal five hands of five cards each" do
+    num_hands = 5
+    num_cards = 5
+    deck = Poker.new_deck() |> Poker.shuffle_deck()
+    deck_size = Enum.count(deck)
+    {hands, deck} = Poker.deal(num_hands, deck, num_cards)
+    assert Enum.count(deck) == deck_size - num_hands * num_cards
+    assert Enum.count(hands) == num_hands
+  end
+
+  test "hand rank percentages are close to wikipedia percentages" do
+    sample_size = 1
+    counts = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    Enum.each(1..sample_size, fn _ ->
+      deck = Poker.new_deck() |> Poker.shuffle_deck()
+      {hands, _} = Poker.deal(10, deck)
+
+      Enum.each(hands, fn hand ->
+        ranking = Poker.hand_rank(hand)
+        # ^counts = List.update_at(counts, Kernel.elem(ranking, 0), &(&1 + 1))
+      end)
+    end)
+  end
+end
